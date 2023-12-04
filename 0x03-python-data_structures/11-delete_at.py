365a21ddf55d078263469f8601cef4f83de69906
Void-Individual <otayomitv@gmail.com>
2023-12-04 10:55:29 +0100

    incorrect script on line 1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
     count = len(my_list)
     if idx > count:
         return my_list
-    my_list.remove(my_list[idx])
+    del my_list[idx]
     return my_list
