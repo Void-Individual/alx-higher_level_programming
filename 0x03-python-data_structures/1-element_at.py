cefa1cf46a98296908c6df519de8493276af901a
Void-Individual <otayomitv@gmail.com>
2023-12-04 11:13:14 +0100

    ugh, had to update idx comp values

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     count = len(my_list)
-    if idx > count or count == 0:
+    if idx > count - 1:
         return None
     return my_list[idx]
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
     count = len(my_list)
-    if idx > count or count == 0:
+    if idx > count - 1:
         return my_list
     del my_list[idx]
     return my_list
