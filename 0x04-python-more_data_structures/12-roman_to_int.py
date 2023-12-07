d915f8c99fecec019bf81c2ff795afd95d241152
Void-Individual <otayomitv@gmail.com>
2023-12-07 02:29:04 +0100

    +1000

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,12 @@
 def roman_to_int(roman_string):
     if not roman_string or roman_string.isdigit():
         return 0
-    nume = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+
+    nume = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     count = len(roman_string)
     sum = 0
     for st in range(count):
         key = roman_string[st]
         sum += nume[key]
+
     return sum
