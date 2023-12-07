cdeb93101c39c077a1ccf393cc16e616a39bc043
Void-Individual <otayomitv@gmail.com>
2023-12-07 17:32:38 +0100

    fixed not str

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or roman_string.isdigit():
+    if not roman_string:
         return 0
+    if type(roman_string) is str:
+        nume = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        count = len(roman_string)
+        sum = 0
+        ls_rn = []
+        for st in range(count):
+            key = roman_string[st]
+            if key in nume:
+                ls_rn.append(nume[key])
 
-    nume = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    count = len(roman_string)
-    sum = 0
-    ls_rn = []
-    for st in range(count):
-        key = roman_string[st]
-        ls_rn.append(nume[key])
-
-    count = len(ls_rn)
-    x = 0
-    curr = 0
-    last = 0
-    while x < count:
-        curr = ls_rn[x]
-        if last != 0 and last < curr:
-            sum = sum - last + (curr - last)
-        else:
-            sum += curr
-        x += 1
-        last = curr
-    return sum
+        count = len(ls_rn)
+        x = 0
+        curr = 0
+        last = 0
+        while x < count:
+            curr = ls_rn[x]
+            if last != 0 and last < curr:
+                sum = sum - last + (curr - last)
+            else:
+                sum += curr
+            x += 1
+            last = curr
+        return sum
+    return 0
