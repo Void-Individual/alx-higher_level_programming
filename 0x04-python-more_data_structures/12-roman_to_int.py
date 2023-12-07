f286d8db336fb254983aa6c273946d205cc58cf9
Void-Individual <otayomitv@gmail.com>
2023-12-07 04:00:26 +0100

    will it work for 4?

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,21 @@ def roman_to_int(roman_string):
     nume = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     count = len(roman_string)
     sum = 0
+    ls_rn = []
     for st in range(count):
         key = roman_string[st]
-        sum += nume[key]
+        ls_rn.append(nume[key])
 
+    count = len(ls_rn)
+    x = 0
+    curr = 0
+    last = 0
+    while x < count:
+        curr = ls_rn[x]
+        if last != 0 and last < curr:
+            sum = sum - last + (curr - last)
+        else:
+            sum += curr
+        x += 1
+        last = curr
     return sum
