30496ed4791b89d4b810d7cb0cfec6d4a1393d24
Void-Individual <otayomitv@gmail.com>
2023-12-19 22:34:02 +0100

    value error details

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,9 +8,8 @@ class Square:
     def __init__(self, size=0):
         """a method to instantiate a private instance attribute"""
         if isinstance(size, int):
-            if size >= 0:
-                self.__self = size
-            else:
+            if size < 0:
                 raise ValueError("size must be >= 0")
+            self.__self = size
         else:
             raise TypeError("size must be an integer")
