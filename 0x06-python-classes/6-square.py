ad727d4af23abc5bcb34bf4d5ed8d2fc75630ae0
Void-Individual <otayomitv@gmail.com>
2023-12-19 23:16:24 +0100

    was this happening cause of comments?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,12 +47,14 @@ class Square:
 
     @position.setter
     def position(self, value):
+        """private instance method to set new position"""
         if (isinstance(value, tuple) and isinstance(value[0], int)
                 and isinstance(value[1], int)):
             if value[0] >= 0 and value[1] >= 0:
                 self.__position = value
             else:
-                raise TypeError("position must be a tuple of 2 positive integers")
+                raise TypeError("position must be a tuple"
+                                "of 2 positive integers")
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
