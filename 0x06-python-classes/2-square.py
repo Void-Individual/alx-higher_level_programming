51ce87664f0d1883ef4ddae16a7acac9c57d2e1c
Void-Individual <otayomitv@gmail.com>
2023-12-19 22:22:34 +0100

    changed the < 0 conditions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,9 @@ class Square:
     def __init__(self, size=0):
         """a method to instantiate a private instance attribute"""
         if isinstance(size, int):
-            self.__self = size
+            if size >= 0:
+                self.__self = size
+            else:
+                raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
