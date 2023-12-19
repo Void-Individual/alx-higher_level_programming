b0c85955b44844ca197154e2643db01bc306b6b9
Void-Individual <otayomitv@gmail.com>
2023-12-20 00:36:08 +0100

    I'm gonna stop for now

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -9,8 +9,8 @@ class Node:
             self.__data = data
         else:
             raise TypeError("data must be an integer")
-        if isinstance(next_node, Node):
-            self.next_node = next_node
+        if isinstance(next_node, Node) or next_node == None:
+            self.__next_node = next_node
         else:
             raise TypeError("next_node must be a Node object")
 
@@ -28,3 +28,47 @@ class Node:
             raise TypeError("data must be an integer")
 
     @property
+    def next_node(self):
+        """method to retrieve the next node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """"method to set new next node"""
+        if isinstance(value, Node) or value == None:
+            self.__next_node = value
+        else:
+            raise TypeError("next_node must be a Node object")
+
+class SinglyLinkedList:
+    """class to define a singly linked list"""
+
+    def __init__(self):
+        """no variable to instantiate"""
+        self.head = None
+
+    def sorted_insert(self, value):
+        """insert a new node into correct position"""
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+        elif self.head.data > value:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while (current.next_node != None and
+                   current.next_node.data < value):
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """print the entire lit to stdout"""
+        res = []
+        current = self.head
+        while current:
+            res.append(str(current.data))
+            current = current.next_node
+        return '\n'.join(res)
