e91963cca5b75f4836c293c52e213e1460ced1d3
Void-Individual <otayomitv@gmail.com>
2023-12-19 22:08:33 +0100

    experimenting on the new documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This is a class module"""
+
+
 class Square:
     """An empty class to define a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This is a class module"""
+
+
 class Square:
     """A class to define a square"""
     def __init__(self, size):
