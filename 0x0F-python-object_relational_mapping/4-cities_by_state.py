6778f2595283a650cf7e4b08d7b634d7f34110e5
Void-Individual <otayomitv@gmail.com>
2024-03-17 22:19:17 +0100

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to run a sql script"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id"
+    cursor.execute(query)
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -5,4 +5,4 @@
 - 1-filter_states.py: Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
 - 2-my_filter_states.py: Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
 - 3-my_safe_filter_states.py: This time, write one that is safe from MySQL injections!
-- 
+- 4-cities_by_state.py: 
