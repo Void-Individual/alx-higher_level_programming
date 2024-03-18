70fbacb98f8fdccd33465326f6a43ef04a3e0926
Void-Individual <otayomitv@gmail.com>
2024-03-18 02:23:38 +0100

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to contain a state class"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    """A class that inherits from Base to link a MySQL table"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
