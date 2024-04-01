5c1bd905c4ed21132f9b4574e05d33d2f0b0a4ed
Void-Individual <otayomitv@gmail.com>
2024-04-01 23:39:00 +0100

    isnt running here

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+
+    data = parse.urlencode({'email': email}).encode('utf-8')
+    with request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+        print(body)
