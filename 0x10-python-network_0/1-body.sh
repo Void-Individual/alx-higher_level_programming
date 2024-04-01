27d5ebace0ded28d3ec4c762edadd5211c7563aa
Void-Individual <otayomitv@gmail.com>
2024-04-01 21:06:56 +0100

    All tasks except 6

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-echo "$(curl -s -o dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1")"
+curl -sL -X GET "$1"
