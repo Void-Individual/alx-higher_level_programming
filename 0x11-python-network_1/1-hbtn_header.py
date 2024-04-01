9023a56fcd4eb33da9e3798dec28c8e5ddf5ed25
Void-Individual <otayomitv@gmail.com>
2024-04-01 23:19:31 +0100

    tasks 0 and 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""A script to take in a url, send a request to it and display a specific value"""
+"""A script to take in a url, send a request to it
+and display a specific value"""
 
 from urllib import request
 from sys import argv
+
 
 if __name__ == "__main__":
     with request.urlopen(argv[1]) as response:
